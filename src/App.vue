9f54c4e827641b8d766a693d6f2bef7a0d3c3e04
Nhellu <pathza.2305@gmail.com>
2024-06-04 21:02:11 +0700

    fix: profile picture

--- src/App.vue
+++ src/App.vue
@@ -178,7 +178,7 @@
             <div data-theme="dim" class="chat chat-end bg-white mt-2">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
             </div>
 
